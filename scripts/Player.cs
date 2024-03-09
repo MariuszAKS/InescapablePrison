67c352a0a1a481942f822bd080bb3f5ad44c5f1b
MariuszAKS <mariusz-k-sikora@o2.pl>
2024-03-09 08:25:51 +0100

    Player movement and wall collision

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -3,24 +3,23 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-	[Export] private float _runSpeed = 300.0f;
-	[Export] private float _walkSpeed = 300.0f;
-	[Export] private float _sneakSpeed = 300.0f;
+	private float _runSpeed = 100.0f;
+	private float _walkSpeed = 50.0f;
+	private float _sneakSpeed = 20.0f;
 
 
 
 	public override void _PhysicsProcess(double delta)
 	{
-		Vector2 velocity = Velocity;
+		HandleMovement();
+	}
 
+	private void HandleMovement()
+	{
 		Vector2 direction = Input.GetVector("move_left", "move_right", "move_up", "move_down");
 		float speed = Input.IsActionPressed("move_sneak") ? _sneakSpeed : Input.IsActionPressed("move_run") ? _runSpeed : _walkSpeed;
 
-		velocity.X = direction == Vector2.Zero ? Mathf.MoveToward(Velocity.X, 0, speed) : direction.X * speed;
-
-
-
-		Velocity = velocity;
+		Velocity = direction * speed;
 		MoveAndSlide();
 	}
 }
